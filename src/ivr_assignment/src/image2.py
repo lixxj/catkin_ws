edebd1ab2ed0dfb36d4f67c55d26df0a4c2ad456
XJ <z5190719@ad.unsw.edu.au>
2019-11-07 23:18:46 +0000

    Update image2.py

--- src/ivr_assignment/src/image2.py
+++ src/ivr_assignment/src/image2.py
@@ -10,7 +10,6 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Float64MultiArray, Float64
 from cv_bridge import CvBridge, CvBridgeError
 
-
 class image_converter:
 
   # Defines publisher and subscriber
@@ -23,7 +22,6 @@ class image_converter:
     self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
-
 
   # Recieve data, process it, and publish
   def callback2(self,data):
@@ -57,5 +55,3 @@ def main(args):
 # run the code if the node is called
 if __name__ == '__main__':
     main(sys.argv)
-
-
