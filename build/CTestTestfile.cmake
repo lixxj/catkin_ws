4dab76c0f3a14898b2ffe024509daa64e21b7455
lixxj <xingjian.li1@student.unsw.edu.au>
2019-11-07 21:36:13 +0000

    Uploaded starter files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dice/catkin_ws/src
+# Build directory: /home/dice/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ivr_assignment")
