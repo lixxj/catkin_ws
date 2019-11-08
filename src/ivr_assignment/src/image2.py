f6e8938a76e87b33763a32ed29e70701e431c3b5
XJ Li <49367624+lixxj@users.noreply.github.com>
2019-11-08 00:35:01 +0000

    Update image2.py

--- src/ivr_assignment/src/image2.py
+++ src/ivr_assignment/src/image2.py
@@ -31,9 +31,6 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
     
-    # Uncomment if you want to save the image
-    #cv2.imwrite('image_copy.png', cv_image)
-    
     im2=cv2.imshow('window2', self.cv_image2)
     cv2.waitKey(1)
 
