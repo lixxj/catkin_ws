f504956f9f2fa3ba2e0053ae9646eacfd9316f63
Xj Li <z5190719@ad.unsw.edu.au>
2019-11-11 13:53:26 +0000

    Finished image1 refactoring

--- src/ivr_assignment/src/image1.py
+++ src/ivr_assignment/src/image1.py
@@ -10,7 +10,6 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Float64MultiArray, Float64
 from cv_bridge import CvBridge, CvBridgeError
 
-
 class image_converter:
 
   # Defines publisher and subscriber
@@ -25,55 +24,56 @@ class image_converter:
     self.blob_pub1 = rospy.Publisher("/camera1/blob_pos",Float64MultiArray, queue_size=10)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
-    
+
+############### END OF COLOUR DECTECTION FUNCTIONS ###############
   def detect_red(self,image):
-      # Isolate the blue colour in the image as a binary image
+      # Isolate the colour region
       mask = cv2.inRange(image, (0, 0, 100), (0, 0, 255))
-      # This applies a dilate that makes the binary region larger (the more iterations the larger it becomes)
+      # Dilate the region 
       kernel = np.ones((5, 5), np.uint8)
       mask = cv2.dilate(mask, kernel, iterations=3)
-      # Obtain the moments of the binary image
+      # Obtain the moments of the region
       M = cv2.moments(mask)
-      # Calculate pixel coordinates for the centre of the blob
-      if M['m00'] == 0:
-	return np.array([np.nan,np.nan])
+      # Calculate pixel coordinates
+      if M['m00'] == 0: # colour not found
+	      return np.array([np.nan,np.nan])
       cx = int(M['m10'] / M['m00'])
       cy = int(M['m01'] / M['m00'])
       return np.array([cx, cy])
-  # Detecting the centre of the green circle
+  
   def detect_green(self,image):
       mask = cv2.inRange(image, (0, 100, 0), (0, 255, 0))
       kernel = np.ones((5, 5), np.uint8)
       mask = cv2.dilate(mask, kernel, iterations=3)
       M = cv2.moments(mask)
       if M['m00'] == 0:
-	return np.array([np.nan,np.nan])
+	      return np.array([np.nan,np.nan])
       cx = int(M['m10'] / M['m00'])
       cy = int(M['m01'] / M['m00'])
       return np.array([cx, cy])
-  # Detecting the centre of the blue circle
+  
   def detect_blue(self,image):
       mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
       kernel = np.ones((5, 5), np.uint8)
       mask = cv2.dilate(mask, kernel, iterations=3)
       M = cv2.moments(mask)
       if M['m00'] == 0:
-	return np.array([np.nan,np.nan])
+	      return np.array([np.nan,np.nan])
       cx = int(M['m10'] / M['m00'])
       cy = int(M['m01'] / M['m00'])
       return np.array([cx, cy])
-  # Detecting the centre of the yellow circle
+  
   def detect_yellow(self,image):
       mask = cv2.inRange(image, (0, 100, 100), (0, 255, 255))
       kernel = np.ones((5, 5), np.uint8)
       mask = cv2.dilate(mask, kernel, iterations=3)
       M = cv2.moments(mask)
       if M['m00'] == 0:
-	return np.array([np.nan,np.nan])
+	      return np.array([np.nan,np.nan])
       cx = int(M['m10'] / M['m00'])
       cy = int(M['m01'] / M['m00'])
       return np.array([cx, cy])
-
+############### END OF COLOUR DECTECTION FUNCTIONS ###############
 
   # Recieve data from camera 1, process it, and publish
   def callback1(self,data):
@@ -83,13 +83,13 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
     
-    self.blob_pos1=Float64MultiArray()
-    self.blob_pos1.data=np.array([self.detect_yellow(self.cv_image1),self.detect_blue(self.cv_image1),
-				  self.detect_green(self.cv_image1),self.detect_red(self.cv_image1)]).flatten()
-        
+    self.blob_pos1 = Float64MultiArray()
+    self.blob_pos1.data = np.array([self.detect_yellow(self.cv_image1),self.detect_blue(self.cv_image1),
+				  self.detect_green(self.cv_image1),self.detect_red(self.cv_image1)]).flatten()        
 
     im1=cv2.imshow('window1', self.cv_image1)
-    cv2.waitKey(1)
+    cv2.waitKey(10000)
+    
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
@@ -109,4 +109,3 @@ def main(args):
 # run the code if the node is called
 if __name__ == '__main__':
     main(sys.argv)
-
