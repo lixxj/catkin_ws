035ed2518965111c5992d949dab8d2c585fbfb06
XJ Li <z5190719@ad.unsw.edu.au>
2019-11-29 00:12:54 +0000

    Modified file locations

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dice/catkin_ws/src
-# Build directory: /home/dice/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ivr_assignment")
--- build/ivr_assignment/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dice/catkin_ws/src/ivr_assignment
-# Build directory: /home/dice/catkin_ws/build/ivr_assignment
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
